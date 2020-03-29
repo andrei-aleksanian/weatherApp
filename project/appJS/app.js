0c0696370168a70d11448f6c271653f7a2ce6d66
andrei-aleksanian <aleksanian.andrei@gmail.com>
2020-03-29 21:43:13 +0100

    Promises practice done

--- project/appJS/app.js
+++ project/appJS/app.js
@@ -1,18 +1,82 @@
 const cityForm = document.getElementById("cityForm");
-const content_div = document.getElementById("content");
+const city_div = document.getElementById("city");
 
-const hide_question = () =>{
-    content_div.classList.add("hide");
+const question_layout = () =>{
+    city_div.classList.add("hide");
+    cityForm.children[0].children[1].classList.remove("hide");
+    cityForm.children[0].children[0].classList.add("hide");
 };
 
-const display_question = () =>{
+const weather_layout = () =>{
+    city_div.classList.remove("hide");
+    cityForm.children[0].children[1].classList.add("hide");
+    cityForm.children[0].children[0].classList.remove("hide");
+};
+
+cityForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    weather_layout();
+
+
+});
+
+question_layout();
+
+// Learning and Testing code
+
+// const getTodos = (link) => {
+//     return new Promise((resolve, reject) => {
+//         const request = new XMLHttpRequest();
+//
+//         request.addEventListener("readystatechange", () => {
+//             if (request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 resolve(data);
+//             } else if (request.readyState === 4) {
+//                 reject("error");
+//             }
+//         });
+//
+//         request.open("GET", link);
+//         request.send();
+//     });
+// };
+
+const getTodos = async () =>{
+
+    const response = await fetch("https://jsonplaceholder.typicode.chom/posts");
+
+    if(response.status !== 200){
+        throw new Error("cannot fetch the data");
+    }
+
+    const data = await response.json();
+
+    return data;
 
 };
 
-const hide_weather = () =>{
+console.log(1);
+console.log(2);
 
-};
+// getTodos("https://jsonplaceholder.typicode.com/posts")
+//     .then(data =>{
+//         console.log(data);
+//         return getTodos("https://jsonplaceholder.typicode.com/posts")
+//     }).then(data =>{
+//         console.log(data);
+//     }).catch(err =>{
+//         console.log(err);
+// });
 
-const display_weather = () =>{
+getTodos().then(data =>{
+    console.table(data);
+}).catch(err =>{
+    console.log(err.message);
+});
 
-};
+
+
+console.log(3);
+console.log(4);
+
