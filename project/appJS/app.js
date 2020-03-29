556e30270463dedc3a9bafcc047f903618dee230
andrei-aleksanian <aleksanian.andrei@gmail.com>
2020-03-29 15:36:12 +0100

    Version of HTML CSS ready to be changed with JS.

---
+++ project/appJS/app.js
@@ -0,0 +1,18 @@
+const cityForm = document.getElementById("cityForm");
+const content_div = document.getElementById("content");
+
+const hide_question = () =>{
+    content_div.classList.add("hide");
+};
+
+const display_question = () =>{
+
+};
+
+const hide_weather = () =>{
+
+};
+
+const display_weather = () =>{
+
+};
